8eb87e08cf60e5495bdefe2cbf13c5bf0b8aca38
Luis Sibson <2260702s@student.gla.ac.uk>
2019-03-05 16:10:22 +0000

    chapter 7 done

--- glasgowgems/urls.py
+++ glasgowgems/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^admin/', admin.site.urls),
     url(r'^add_category/$',views.add_category,name='add_category'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_gem/$', 
-        views.add_gem, name='add_gem'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
 		views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_gem/$', 
+        views.add_gem, name='add_gem'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
